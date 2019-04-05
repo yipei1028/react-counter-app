ab197d8a3dfe7b5b4eafa99f2bf54d2597c3c5dc
Yi-Pei Huang <yipeihuang1028@gmail.com>
2019-04-05 17:30:16 +0800

    First commit - good course worth to take it - Hey, React dude. I am back again lol

---
+++ src/components/counters.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+  render() {
+    const { onReset, onDelete, onIncrement } = this.props;
+    return (
+      <div>
+        <button 
+          onClick={onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        { this.props.counters.map(counter => (
+          <Counter 
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+ 
+export default Counters;
